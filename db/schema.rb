25f6fd78b9b2fdbca7c33fd3a9466cd538062ddf
ViktoriaB2018 <bvo2008@mail.ru>
2018-09-30 21:54:21 +0300

    Change migrate posts & display posts on main page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_165625) do
+ActiveRecord::Schema.define(version: 2018_09_30_184812) do
 
   create_table "comments", force: :cascade do |t|
     t.text "author"
