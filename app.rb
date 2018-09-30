ad9a244fd72b4b8ce31a565b49f9718d4afa6250
ViktoriaB2018 <bvo2008@mail.ru>
2018-09-30 20:17:30 +0300

    Add new posts to database & validation

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:talk_about.db"
 
 class Post < ActiveRecord::Base
+	validates :author, presence: true, length: { minimum: 3 }
+	validates :title, presence: true
+	validates :content, presence: true
 end
 
 class Comment < ActiveRecord::Base
@@ -17,11 +20,18 @@ get '/' do
 end
 
 get '/new' do
+	@p = Post.new
 	erb :new		
 end
 
 post '/new' do
-	
+	@p = Post.new params[:post]
+	if @p.save
+		erb "<h4>Post was created.</h4>"
+	else
+		@error = @p.errors.full_messages.first
+		erb :new
+	end
 end
 
 get '/authors' do
